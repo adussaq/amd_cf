4a7a783b7f1f5d2e5126854d85def006ae107565
Alex <a.m.dussaq@gmail.com>
2015-08-04 13:38:41 -0500

    add in package for managing equation submission

---
+++ fitCurves.js
@@ -0,0 +1,99 @@
+/*global self, window, Math, amd_cf*/
+/*jslint todo: true */
+
+var amd_cf = (function () {
+    'use strict';
+    var main, $, checkjQ, checkWW, worker, workerScript, checkdata, loaded;
+
+    //Note: This indicates that this package and fitCurvesWorkers must be in the same
+    // Directory, if they are not, this location needs to be changed...
+    workerScript = 'fitCurvesWorker.js';
+    main = {};
+    loaded = false;
+
+    checkjQ = function () {
+        var ret;
+        if (window.jQuery) {
+            $ = window.jQuery;
+            ret = true;
+        } else {
+            console.error('jQuery is required for this add on package.');
+            ret = false;
+        }
+        return ret;
+    };
+
+    checkWW = function () {
+        var ret;
+        if (window.amd_ww) {
+            worker = window.amd_ww.startWorkers({filename: workerScript});
+            checkWW = function () {
+                return true;
+            };
+            ret = true;
+        } else {
+            console.error('amd_ww is required for this curve fitting, download at: https://github.com/adussaq/amd_ww/');
+            ret = false;
+        }
+        return ret;
+    };
+
+    checkdata = function (data) {
+        var ret = true;
+        if (!data.hasOwnProperty(x_values)) {
+            console.error('Must inlcude x_values as an property on data');
+            ret = false;
+        }
+        if (!data.y_values) {
+            console.error('Must inlcude y_values as an property on data');
+            ret = false;
+        }
+        if (!data.equation) {
+            console.error('Must inlcude equation as an property on data');
+            ret = false;
+        }
+        if (ret) {
+            if (data.x_values.constructor !== Array || data.y_values.constructor !== Array) {
+                console.error('Both x_values and y_values must be arrays');
+                ret = false;
+            }
+            if (ret && data.x_values.length !== data.y_values.length) {
+                console.error('The length of x_values and y_values must be the same');
+                ret = false;
+            if (typeof data.x_values[0].constructor !== Array) {
+                console.error('data.x_values must be an array of arrays, each array represents the X vector for each point.');
+                ret = false;
+            }
+            if (!data.hasOwnProperty(equation) || typeof data.equation !== 'Object' || !loaded) {
+                console.error('Equation must be loaded using "getEquation" function first');
+            }
+
+        }
+    };
+
+    main.getEquation = function (url, callback) {
+        if (checkjQ()) {
+            if (!callback || typeof callback !== 'function') {
+                console.error('Callback function should be defined and utilized, this is done asynchronously.');
+                callback = function (eq) {
+                    loaded = true;
+                    console.log('equation loaded', eq);
+                };
+            }
+            $.get(url, function (res) {
+                console.log(res);
+            });
+        }
+    };
+
+    main.fitEquation = function (data, callback) {
+        if (checkWW()) {
+            if (checkdata(data)) {
+                worker.submitJob(data, callback);
+            }
+        }
+    }
+
+    return main;
+
+}());
